d7920984653563e535a094e5bee13a56c8f5f078
Sakib Ahmed <sakibahmad99090@gmail.com>
2021-02-14 22:22:20 +0600

    some changes added

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const showImages = (images) => {
 }
 
 const getImages = (query) => {
-  // imagesArea.style.display = 'block';
+
   displaySpinner()
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
